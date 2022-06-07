e9d08483f8e320007f113388a0354fdc1d3dbca5
Hugoll-1 <101283981+Hugoll-1@users.noreply.github.com>
2022-06-07 08:30:40 +0200

    Ecriture de l'interface Graphe

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Interface Graphe
+ */
 public interface Graphe {
+
+    /**
+     * methode listeNoeuds
+     * @return tous les noeuds du graphe
+     */
+    public List<String> listeNoeuds();
+
+    /**
+     * Methode suivant
+     * @param n le nom du noeud
+     * @return la liste des arcs partant du noeud n
+     */
+    public List<Arc> suivants(String n);
 }
