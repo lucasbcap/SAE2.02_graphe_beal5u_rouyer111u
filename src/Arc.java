e436b8bd6a9be445eebf2b3cb04c8ca3bfa61f6e
lucasbcap <beal.lucas2002@gmail.com>
2022-06-07 08:29:30 +0200

    Creation de noeud

--- src/Arc.java
+++ src/Arc.java
@@ -11,8 +11,31 @@ public class Arc {
      * @param d le nom d'un noeud
      * @param c le poid d'un noeud
      */
-    public Arc(String d, int c){
+    public Arc(String d, double c){
         this.dest = d;
         this.cout = c;
+    }
+
+
+    /////////////////////////
+    //
+    // GETTER
+    //
+    /////////////////////////
+
+    /**
+     * getter pour la valeur de l arc
+     * @return la valeur de l arc
+     */
+    public double getCout() {
+        return cout;
+    }
+
+    /**
+     * getter pour le noeud suivant
+     * @return le noeud suivant
+     */
+    public String getDest() {
+        return dest;
     }
 }
--- src/Noeud.java
+++ src/Noeud.java
@@ -1,2 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe Noeud
+ */
 public class Noeud {
+
+    //Variables
+
+    private String nom;
+    private List<Arc> adj;
+
+    /**
+     * Constructeur de noeud avec un nom en parametre
+     * @param nom nom du noeud
+     */
+    public Noeud (String nom){
+        this.nom = nom;
+        this.adj = new ArrayList<Arc>();
+    }
+
+    /**
+     * Deux noeud sont egaux si ils ont le meme nom
+     * @param o l objet noeud
+     * @return
+     */
+    public boolean equals(Object o){
+        Noeud n = (Noeud)o;
+        return this.nom.compareTo(n.getNom())==0;
+    }
+
+    /**
+     * permet d ajouter un arc a la liste adj
+     * @param destination prochain noeud
+     * @param cout la valeur de l arc
+     */
+    public void ajouterArc(String destination, double cout){
+        Arc a = new Arc(destination,cout);
+        this.adj.add(a);
+    }
+
+    /////////////////////////
+    //
+    // GETTER
+    //
+    /////////////////////////
+
+
+    /**
+     * getter pour avoir le nom
+     * @return le nom du noeud
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * getter pour la liste des Arcs
+     * @return la liste des Arcs
+     */
+    public List<Arc> getAdj() {
+        return adj;
+    }
 }
