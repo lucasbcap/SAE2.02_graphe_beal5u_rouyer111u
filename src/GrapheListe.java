0e4b5ff91cb246e78055d8704430fb2ad095b358
lucasbcap <beal.lucas2002@gmail.com>
2022-06-07 08:35:02 +0200

    Creation de GrapheListe

--- src/GrapheListe.java
+++ src/GrapheListe.java
@@ -1,2 +1,40 @@
-public class GrapheListe {
+import java.util.List;
+
+public class GrapheListe implements Graphe{
+
+    private List<String> ensNom;
+    private List<Noeud> ensNoeuds;
+
+    /**
+     *
+     */
+    public GrapheListe(){
+
+    }
+
+
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public List<String> listeNoeuds() {
+        return null;
+    }
+
+    /**
+     *
+     * @param n le nom du noeud
+     * @return
+     */
+    @Override
+    public List<Arc> suivants(String n) {
+        return null;
+    }
+
+
+    public void  ajouterArc(String depart,String destination,double cout){}
+
+
 }
