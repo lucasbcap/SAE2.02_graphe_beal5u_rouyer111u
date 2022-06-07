b2e8a9709a9efdc9970dd0c3aba9bf6d87e2894d
Hugoll-1 <101283981+Hugoll-1@users.noreply.github.com>
2022-06-07 08:23:36 +0200

    Ecriture de la classe Arc

--- src/Arc.java
+++ src/Arc.java
@@ -1,2 +1,18 @@
+/**
+ * Classe Arc
+ */
 public class Arc {
+
+    private String dest;
+    private double cout;
+
+    /**
+     * Constructeur de la classe Arc
+     * @param d le nom d'un noeud
+     * @param c le poid d'un noeud
+     */
+    public Arc(String d, int c){
+        this.dest = d;
+        this.cout = c;
+    }
 }
