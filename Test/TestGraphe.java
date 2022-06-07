25f34c7b4e2a8519c01486c806050adbf220cb9c
Hugoll-1 <101283981+Hugoll-1@users.noreply.github.com>
2022-06-07 09:54:36 +0200

    Ajout de test

---
+++ Test/TestGraphe.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+
+
+public class TestGraphe {
+
+
+    @Test
+    public void testSuivantExistant(){
+
+    }
+
+    @Test
+    public void testSuivantInexistant(){
+
+    }
+
+    @Test
+    public void testAjouterArc(){
+
+    }
+
+
+    @Test
+    public void testTailleListeNoeud(){
+
+    }
+
+
+
+
+}
