b07c5ff6f650d210fef5b5fb560687706c03b051
Hugoll-1 <101283981+Hugoll-1@users.noreply.github.com>
2022-06-13 11:19:46 +0200

    Ajout des graphes

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+
 public class Dijkstra {
 
     private Graphe g;
     private String a;
 
+    /**
+     * Constructeur de l'algo de Dijkstra
+     * @param graphe un graphe
+     * @param depart le sommet de depart
+     */
     public Dijkstra( Graphe graphe, String depart){
+        this.g = graphe;
+        this.a = depart;
 
+        ArrayList<Integer> q = new ArrayList<>();
+        for
     }
+
+
 }
