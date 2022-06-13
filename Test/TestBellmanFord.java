f5b45a7a2675a871be15183f9aaf5f6ad42e196b
Hugoll-1 <101283981+Hugoll-1@users.noreply.github.com>
2022-06-13 10:54:55 +0200

    Ajout de classe

---
+++ Test/TestBellmanFord.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestBellmanFord {
+
+    @Test
+    public void testValeurMin(){
+
+    }
+
+    @Test
+    public void testBoucle(){
+
+    }
+
+
+
+
+}
---
+++ src/Dijkstra.java
@@ -0,0 +1,9 @@
+public class Dijkstra {
+
+    private Graphe g;
+    private String a;
+
+    public Dijkstra( Graphe graphe, String depart){
+
+    }
+}
