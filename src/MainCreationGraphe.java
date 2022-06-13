bfabeeb99ff85b6913a72ff4ad368aba2355954d
lucasbcap <beal.lucas2002@gmail.com>
2022-06-13 17:41:33 +0200

    creation MainCreationGraphe

--- src/MainCreationGraphe.java
+++ src/MainCreationGraphe.java
@@ -1,9 +1,42 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MainCreationGraphe {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        GenererGraphe gg = new GenererGraphe(50, "0", "5");
-        GrapheListe gl = gg.creerGraphe();
-        System.out.println(gl.toGraphViz());
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter("resultats/BellmanFord.txt"));
+
+        for(int i=10 ; i < 500;i++){
+
+            GenererGraphe gg = new GenererGraphe(50, "0", "4");
+            GrapheListe gl = gg.creerGraphe();
+            BellmanFord bf = new BellmanFord();
+            long debut = System.currentTimeMillis();
+            Valeur v = bf.resoudre(gl,"0");
+            long fin = System.currentTimeMillis();
+            bw.write (Double.toString((fin-debut)/1000.0));
+            bw.newLine();
+        }
+        bw.close();
+
+
+        BufferedWriter bw2 = new BufferedWriter(new FileWriter("resultats/Dijkstra.txt"));
+
+        for(int i=10 ; i < 500;i++){
+
+            GenererGraphe gg = new GenererGraphe(50, "0", "4");
+            GrapheListe gl = gg.creerGraphe();
+            Dijkstra bf = new Dijkstra();
+            long debut = System.currentTimeMillis();
+            Valeur v = bf.resoudre(gl,"0");
+            long fin = System.currentTimeMillis();
+            bw2.write (Double.toString((fin-debut)/1000.0));
+            bw2.newLine();
+        }
+        bw2.close();
+
     }
 }
